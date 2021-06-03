32575e4a1abdab61895e279cbbc8af5353f93b40
Mohmad <magomedov951@mail.ru>
2021-06-03 14:25:58 +0300

    lala

--- models/Category.js
+++ models/Category.js
@@ -6,3 +6,11 @@ const schema = new Schema({
 })
 
 module.exports = model('Category', schema)
+const { Schema, model } = require('mongoose');
+const schema = new Schema({
+    name: String,
+    createdAt: Date,
+    updateAt: Date
+})
+
+module.exports = model('Category', schema)
--- models/Comment.js
+++ models/Comment.js
@@ -10,3 +10,15 @@ const Comments = mongoose.Schema({
 })
 
 module.exports = mongoose.model("comment", Comments)
+const mongoose = require('mongoose');
+
+const Comments = mongoose.Schema({
+  text:String,
+  postId: { type: mongoose.Types._ObjectId, ref:"Post" },
+  author: String,
+  createdAt:Date,
+  updatedAt:Date
+
+})
+
+module.exports = mongoose.model("comment", Comments)
